0385af2847eea7e04225e266c6ffa64a7703999d
najar1412 <rory.jarrel@gmail.com>
2018-07-10 17:26:48 -0400

    added columns in landing page

--- rendertools/rorender/models.py
+++ rendertools/rorender/models.py
@@ -5,8 +5,10 @@ class Machine(models.Model):
     ip = models.CharField(max_length=200)
     port = models.CharField(max_length=200)
     running = models.BooleanField(default=True)
+    rendering = models.BooleanField(default=False)
     vray_running = models.BooleanField(default=False)
     corona_running = models.BooleanField(default=False)
+    is_workstation = models.BooleanField(default=False)
 
     def __str__(self):
         return f'<{self.name}({self.ip})>'
